59dc865c6093c08b6e54553c08bc0e85f1281493
hugoboosh <hugoboosh@yahoo.com>
2018-01-03 19:37:09 -0500

    Update: Fixed Type in GENESIS_COINBASE_TX_HEX

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,8 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "YetiCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494f
-fbbd0880712101599afcbc1ce8433418b05f28979b6d198c6bae7a4bb49746e3ff86d6e4d00ff8";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101599afcbc1ce8433418b05f28979b6d198c6bae7a4bb49746e3ff86d6e4d00ff8";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
