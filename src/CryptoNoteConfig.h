9a4cd2505e8eb13f4848f46455fe3d758f9bd9e9
hugoboosh <hugoboosh@yahoo.com>
2018-01-03 19:33:13 -0500

    Update:  Fixed URL issue, added GENESIS_COINBASE_TX_HEX value after initial build.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "YetiCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494f
+fbbd0880712101599afcbc1ce8433418b05f28979b6d198c6bae7a4bb49746e3ff86d6e4d00ff8";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
@@ -106,7 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-10.128.0.2:4573  //"your_seed_ip1.com:8080",
+"yeticoin.tk:4573"  //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
 
